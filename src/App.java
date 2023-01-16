7d0d04403f4e29e949fd0d1ed9ff706ca23fbab8
Thyago Lucas <thyago.devin@gmail.com>
2023-01-16 12:45:49 -0400

    feat: finalização, entrega do exercicio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        String[] names = {"maria", "joana", "ana", "leticia", "paula"};
+        String[] names2 = {"julia", "flavia", "alice", "luana", "olivia", "paula"};
+
+        for(int i = 0; i < names.length; i++)
+            for(int j = 0; j < names2.length; j++)
+
+                if(names[i]== names2[j]) 
+                System.out.println(names2[j]);
+            
+
     }
 }
